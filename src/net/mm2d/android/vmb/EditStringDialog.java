98e27c55cb9d23bed850e3101a3c63aed0c4104d
OHMAE Ryosuke <ryo@mm2d.net>
2015-01-24 23:59:57 +0900

    画面サイズ毎のレイアウト調整

--- src/net/mm2d/android/vmb/EditStringDialog.java
+++ src/net/mm2d/android/vmb/EditStringDialog.java
@@ -4,14 +4,19 @@
 
 package net.mm2d.android.vmb;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.TypedValue;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * 文字列編集を行うダイアログ。
@@ -67,26 +72,44 @@ public class EditStringDialog extends DialogFragment {
         }
     }
 
+    @SuppressLint("InflateParams")
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final Bundle args = getArguments();
         final String string = args.getString(KEY_STRING);
-        mEditText = new EditText(getActivity());
+        final LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View view = inflater.inflate(R.layout.dialog_edit, null, false);
+        mEditText = (EditText) view.findViewById(R.id.editText);
         mEditText.setText(string);
-        mEditText.setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                getActivity().getResources().getDimension(R.dimen.edit_font_size));
+        mEditText.setSelection(string.length());
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                final int keyCode = event == null ? -1 : event.getKeyCode();
+                if (actionId == EditorInfo.IME_ACTION_DONE || keyCode == KeyEvent.KEYCODE_ENTER) {
+                    inputText();
+                    dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(getActivity().getString(R.string.string_edit));
         builder.setView(mEditText);
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                if (mEventListener != null) {
-                    mEventListener.onConfirmString(mEditText.getText().toString());
-                }
+                inputText();
             }
         });
         return builder.create();
+    }
+
+    private void inputText() {
+        if (mEventListener != null) {
+            mEventListener.onConfirmString(mEditText.getText().toString());
+        }
     }
 
     @Override
