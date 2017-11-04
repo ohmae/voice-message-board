9370cb4c69e01d50481ffef1024c2d349e5cbfa2
OHMAE Ryosuke <ryo@mm2d.net>
2017-11-04 23:28:21 +0900

    ready for Android Studio 3.0

--- app/src/main/java/net/mm2d/android/vmb/PauseHandler.java
+++
@@ -1,9 +0,0 @@
-package net.mm2d.android.vmb;
-
-import android.os.Handler;
-
-/**
- * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
- */
-public class PauseHandler extends Handler {
-}
--- app/src/main/java/net/mm2d/android/vmb/SpeechRecognizerDialog.java
+++
@@ -1,9 +0,0 @@
-package net.mm2d.android.vmb;
-
-import android.support.v4.app.DialogFragment;
-
-/**
- * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
- */
-public class SpeechRecognizerDialog extends DialogFragment {
-}
