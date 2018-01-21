a27aff2527ee0ee4ff82191e016f6fa2ea0b8a14
OHMAE Ryosuke <ryo@mm2d.net>
2018-01-21 10:05:58 +0900

    copyright

--- app/src/main/java/net/mm2d/android/vmb/dialog/DialogFragmentBase.kt
+++ app/src/main/java/net/mm2d/android/vmb/dialog/DialogFragmentBase.kt
@@ -1,10 +1,17 @@
+/*
+ * Copyright (c) 2018 大前良介(OHMAE Ryosuke)
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/MIT
+ */
+
 package net.mm2d.android.vmb.dialog
 
 import android.support.v4.app.DialogFragment
 import android.support.v4.app.FragmentManager
 
 /**
- * @author <a href="mailto:ryo@mm2d.net">大前良介 (OHMAE Ryosuke)</a>
+ * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 open class DialogFragmentBase : DialogFragment() {
     fun showAllowingStateLoss(manager: FragmentManager?, tag: String) {
