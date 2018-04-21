d1ff20827e0a946ba48b4ac3f360e349446029f4
OHMAE Ryosuke <ryo@mm2d.net>
2018-04-21 18:58:38 +0900

    refactor: join activity

--- app/src/main/java/net/mm2d/android/vmb/SettingsActivity.kt
+++ app/src/main/java/net/mm2d/android/vmb/SettingsActivity.kt
@@ -7,21 +7,131 @@
 
 package net.mm2d.android.vmb
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.preference.ListPreference
+import android.preference.Preference
+import android.preference.PreferenceManager
+import android.view.MenuItem
+import net.mm2d.android.vmb.font.FontFileChooserActivity
+import net.mm2d.android.vmb.settings.Key
+import net.mm2d.android.vmb.settings.Settings
 
 /**
  * @author [大前良介 (OHMAE Ryosuke)](mailto:ryo@mm2d.net)
  */
 class SettingsActivity : AppCompatPreferenceActivity() {
+    private lateinit var settings: Settings
+    private lateinit var fontPathPreference: Preference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        fragmentManager.beginTransaction()
-                .replace(android.R.id.content, SettingsFragment())
-                .commit()
+        settings = Settings(this)
+        setUpSetting()
     }
 
-    override fun onIsMultiPane(): Boolean = false
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
-    override fun isValidFragment(fragmentName: String): Boolean = true
+    private fun setUpSetting() {
+        addPreferencesFromResource(R.xml.preferences)
+        bindPreference(findPreference(Key.SCREEN_ORIENTATION))
+        findPreference(Key.PLAY_STORE)?.setOnPreferenceClickListener {
+            openUrl(MARKET_URL)
+        }
+        findPreference(Key.PRIVACY_POLICY)?.setOnPreferenceClickListener {
+            openUrl(PRIVACY_POLICY_URL)
+        }
+        findPreference(Key.SOURCE_CODE)?.setOnPreferenceClickListener {
+            openUrl(SOURCE_CODE_URL)
+        }
+        findPreference(Key.LICENSE)?.setOnPreferenceClickListener {
+            val intent = Intent(this, LicenseActivity::class.java)
+            startActivity(intent)
+            true
+        }
+        findPreference(Key.VERSION_NUMBER)?.summary = BuildConfig.VERSION_NAME
+        setUpFontSetting()
+    }
+
+    private fun setUpFontSetting() {
+        fontPathPreference = findPreference(Key.FONT_PATH)!!
+        fontPathPreference.setOnPreferenceClickListener {
+            startFontChooser()
+            true
+        }
+        setFontPath()
+    }
+
+    private fun setFontPath() {
+        val path = settings.fontPath
+        if (path.isEmpty()) fontPathPreference.setSummary(R.string.pref_description_font_path)
+        else fontPathPreference.summary = path
+    }
+
+    private fun startFontChooser() {
+        val intent = FontFileChooserActivity.makeIntent(this, settings.fontPath)
+        startActivityForResult(intent, FONT_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != FONT_REQUEST_CODE || resultCode != Activity.RESULT_OK) {
+            return
+        }
+        settings.fontPath = data?.data?.path ?: ""
+        setFontPath()
+    }
+
+    private fun findPreference(key: Key): Preference? = super.findPreference(key.name)
+
+    /**
+     * 設定結果を反映させるListenerとの接続。
+     *
+     * @param preference Preference
+     */
+    private fun bindPreference(preference: Preference?) {
+        preference ?: return
+        preference.setOnPreferenceChangeListener(this::bindPreference)
+        val sp = PreferenceManager.getDefaultSharedPreferences(preference.context)
+        val value = sp.getString(preference.key, "")
+        bindPreference(preference, value)
+    }
+
+    private fun bindPreference(preference: Preference, value: Any): Boolean {
+        val stringValue = value.toString()
+        if (preference is ListPreference) {
+            val index = preference.findIndexOfValue(stringValue)
+            preference.summary = if (index >= 0) preference.entries[index] else null
+        } else {
+            preference.summary = stringValue
+        }
+        return true
+    }
+
+    private fun openUrl(url: String): Boolean {
+        val uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            startActivity(intent)
+            return true
+        } catch (ignored: ActivityNotFoundException) {
+        }
+        return false
+    }
+
+    companion object {
+        private const val MARKET_URL = "market://details?id=net.mm2d.android.vmb"
+        private const val SOURCE_CODE_URL = "https://github.com/ohmae/VoiceMessageBoard"
+        private const val PRIVACY_POLICY_URL = "https://github.com/ohmae/VoiceMessageBoard/blob/develop/PRIVACY-POLICY.md"
+        private const val FONT_REQUEST_CODE = 1
+    }
 }
