429c057d90e12e58745d644f0662ecc03f0a5546
OHMAE Ryosuke <ryo@mm2d.net>
2020-01-13 16:12:12 +0900

    ✨fix call super

--- app/src/main/java/net/mm2d/android/vmb/MainActivity.kt
+++ app/src/main/java/net/mm2d/android/vmb/MainActivity.kt
@@ -144,6 +144,7 @@ class MainActivity : AppCompatActivity(),
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         voiceInputDelegate.onActivityResult(requestCode, resultCode, data)
     }
 
