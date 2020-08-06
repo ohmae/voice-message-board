3f928ee95813ff76b3552f39962e8c9f3c0e1d9e
OHMAE Ryosuke <ryo@mm2d.net>
2020-08-06 23:02:21 +0900

    💚format

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,19 +63,19 @@ android {
 val kotlinVersion: String by project
 
 dependencies {
-    implementation ("androidx.appcompat:appcompat:1.1.0")
-    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("androidx.multidex:multidex:2.0.1")
-    implementation ("androidx.preference:preference:1.1.1")
-    implementation ("androidx.core:core-ktx:1.3.1")
-    implementation ("androidx.browser:browser:1.2.0")
-    implementation ("androidx.lifecycle:lifecycle-process:2.2.0")
-    implementation ("com.google.android.material:material:1.2.0")
-    implementation ("com.google.android.play:core:1.8.0")
-    implementation ("com.google.android.play:core-ktx:1.8.0")
-    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.multidex:multidex:2.0.1")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.browser:browser:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-process:2.2.0")
+    implementation("com.google.android.material:material:1.2.0")
+    implementation("com.google.android.play:core:1.8.0")
+    implementation("com.google.android.play:core-ktx:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8")
 
     testImplementation("junit:junit:4.13")
 }
