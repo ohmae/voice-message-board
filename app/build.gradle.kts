fa340b27a0002866a3d05b5069a4e03c1111e074
OHMAE Ryosuke <ryo@mm2d.net>
2021-10-09 20:59:49 +0900

    🆙kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ dependencies {
     implementation("androidx.browser:browser:1.3.0")
     implementation("androidx.lifecycle:lifecycle-process:2.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("com.google.android.play:core:1.10.1")
+    implementation("com.google.android.play:core:1.10.2")
     implementation("com.google.android.play:core-ktx:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
@@ -74,10 +74,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.105.0")
+    debugImplementation("com.facebook.flipper:flipper:0.114.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.105.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.105.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.114.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.114.0")
 }
 
 fun isStable(version: String): Boolean {
