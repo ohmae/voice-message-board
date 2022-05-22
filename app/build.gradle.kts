96bab1ef5bafe4c782afa92f82d2e8443b27b927
OHMAE Ryosuke <ryo@mm2d.net>
2022-05-22 11:50:07 +0900

    🆙flipper

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,10 +77,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    debugImplementation("com.facebook.flipper:flipper:0.145.0")
+    debugImplementation("com.facebook.flipper:flipper:0.146.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.3")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.145.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.145.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.146.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.146.0")
 
     // for release
 }
