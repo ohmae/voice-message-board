aeb4aaf62a8dea088699331719cd0a40bf929f49
OHMAE Ryosuke <ryo@mm2d.net>
2022-08-06 16:25:09 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,20 +74,20 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.core:core-ktx:1.8.0")
     implementation("androidx.browser:browser:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-process:2.5.0")
+    implementation("androidx.lifecycle:lifecycle-process:2.5.1")
     implementation("com.google.android.material:material:1.6.1")
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     testImplementation("junit:junit:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    debugImplementation("com.facebook.flipper:flipper:0.153.0")
+    debugImplementation("com.facebook.flipper:flipper:0.157.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.4")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.153.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.153.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.157.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.157.0")
 
     // for release
 }
