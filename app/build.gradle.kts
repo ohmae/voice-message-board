94d681e27cf97a479ed177955242ce00d2ef0fd7
OHMAE Ryosuke <ryo@mm2d.net>
2024-02-20 22:49:35 +0900

    🆙gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,16 +84,16 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.13")
-    debugImplementation("com.facebook.flipper:flipper:0.245.0")
+    debugImplementation("com.facebook.flipper:flipper:0.247.0")
     debugImplementation("com.facebook.soloader:soloader:0.11.0")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.245.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.245.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.247.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.247.0")
 
     // for release
 }
