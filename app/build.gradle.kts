c5506fb196954026f34e2b6e0fe20b03270d8b80
OHMAE Ryosuke <ryo@mm2d.net>
2026-08-02 19:07:35 +0900

    🆙targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
     defaultConfig {
         applicationId = "net.mm2d.android.vmb"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
         versionName = "$versionMajor.$versionMinor.$versionPatch"
         base.archivesName.set("$applicationName-$versionName")
