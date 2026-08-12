5d5b7e0ac4f280ada9e80d3e29e7c0e10c8a269e
OHMAE Ryosuke <ryo@mm2d.net>
2026-08-12 19:56:26 +0900

    🎉release 1.14.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 val applicationName = "VoiceMessageBoard"
 val versionMajor = 1
 val versionMinor = 14
-val versionPatch = 0
+val versionPatch = 1
 
 android {
     namespace = "net.mm2d.android.vmb"
